d84ac21677d5557ad7f216dc53008635f10269e0
zHoop <gd.dashorrami@gmail.com>
2019-08-01 12:25:47 +0300

    Small changes

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,4 @@
 #include "common.h"
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-
 
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
