01915c1cc24a438440dc113d6fd12291b7cdfb09
zHoop <gd.dashorrami@gmail.com>
2019-08-01 12:25:01 +0300

    Improve compile scripts

--- src/compile_linux.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nasm -felf32 boot.asm -o boot.o
-i686-elf-gcc -c kernel.c common.c -o kernel.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o mountainos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-echo "Run? (Requires QEMU) (y/N)"
-read run 
-if [[ "${run}" == "y" ]]; then
-    qemu-system-i386 -kernel mountainos.bin
-fi 
--- src/compile_windows.bat
+++
@@ -1,6 +0,0 @@
-nasm -felf32 boot.asm -o boot.o
-i686-elf-gcc -c kernel.c common.c -o kernel.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o mountainos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-set /p run="Run? (requires QEMU) (y/n) "
-if %run% EQU y qemu-system-i386 -kernel mountainos.bin
-pause
