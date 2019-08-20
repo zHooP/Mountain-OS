4ded1be51e6768cdd868e998e1040c3a6a6644d8
zHoop <gd.dashorrami@gmail.com>
2019-08-20 15:55:02 +0300

    slight changes to VGA header

--- src/Drivers/VGA.c
+++ src/Drivers/VGA.c
@@ -1,24 +1,5 @@
 #include "VGA.h"
 
-/* Hardware text mode color constants. */
-
-#define VGA_COLOR_BLACK 0
-#define VGA_COLOR_BLUE 1
-#define VGA_COLOR_GREEN 2
-#define VGA_COLOR_CYAN 3
-#define VGA_COLOR_RED 4
-#define VGA_COLOR_MAGENTA 5
-#define VGA_COLOR_BROWN 6
-#define VGA_COLOR_LIGHT_GREY 7
-#define VGA_COLOR_DARK_GREY 8
-#define VGA_COLOR_LIGHT_BLUE 9
-#define VGA_COLOR_LIGHT_GREEN 10
-#define VGA_COLOR_LIGHT_CYAN 11
-#define VGA_COLOR_LIGHT_RED 12
-#define VGA_COLOR_LIGHT_MAGENTA 13
-#define VGA_COLOR_LIGHT_BROWN 14
-#define VGA_COLOR_WHITE 15
-
 static inline uint8_t vga_entry_color(uint8_t fg, uint8_t bg)
 {
 	return fg | bg << 4;
@@ -29,8 +10,8 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 	return (uint16_t)uc | (uint16_t)color << 8;
 }
 
-static const uint8_t VGA_WIDTH = 80;
-static const uint8_t VGA_HEIGHT = 25;
+const uint8_t VGA_WIDTH = 80;
+const uint8_t VGA_HEIGHT = 25;
 
 size_t terminal_row;
 size_t terminal_column;
--- src/Drivers/VGA.h
+++ src/Drivers/VGA.h
@@ -4,6 +4,29 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* Hardware text mode color constants. */
+
+#define VGA_COLOR_BLACK 0
+#define VGA_COLOR_BLUE 1
+#define VGA_COLOR_GREEN 2
+#define VGA_COLOR_CYAN 3
+#define VGA_COLOR_RED 4
+#define VGA_COLOR_MAGENTA 5
+#define VGA_COLOR_BROWN 6
+#define VGA_COLOR_LIGHT_GREY 7
+#define VGA_COLOR_DARK_GREY 8
+#define VGA_COLOR_LIGHT_BLUE 9
+#define VGA_COLOR_LIGHT_GREEN 10
+#define VGA_COLOR_LIGHT_CYAN 11
+#define VGA_COLOR_LIGHT_RED 12
+#define VGA_COLOR_LIGHT_MAGENTA 13
+#define VGA_COLOR_LIGHT_BROWN 14
+#define VGA_COLOR_WHITE 15
+
+extern const uint8_t VGA_WIDTH = 80;
+extern const uint8_t VGA_HEIGHT = 25;
+
+
 static inline uint8_t vga_entry_color(uint8_t fg, uint8_t bg);
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color);
 void terminal_initialize(void);
