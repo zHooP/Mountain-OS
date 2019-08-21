6b6a3dcfd01467f5bac38f7374fba0da783a37e3
zHoop <gd.dashorrami@gmail.com>
2019-08-21 17:57:04 +0300

    Update compile scripts

--- compile_windows.bat
+++ compile_windows.bat
@@ -3,7 +3,8 @@ nasm -felf32 src/boot.asm -o debug/boot.o
 i686-elf-gcc -c src/kernel.c -o debug/kernel.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
 i686-elf-gcc -c src/common.c -o debug/common.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
 i686-elf-gcc -c src/Drivers/keyboard.c -o debug/keyboard.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
-i686-elf-gcc -T src/linker.ld -o debug/mountainos.bin -ffreestanding -O2 -nostdlib debug/boot.o debug/kernel.o debug/common.o debug/keyboard.o -lgcc
+i686-elf-gcc -c src/Drivers/VGA.c -o debug/VGA.o -std=gnu99 -ffreestanding -masm=intel -O2 -Wall -Wextra
+i686-elf-gcc -T src/linker.ld -o debug/mountainos.bin -ffreestanding -O2 -nostdlib debug/boot.o debug/kernel.o debug/common.o debug/keyboard.o debug/VGA.o -lgcc
 set /p run="Run? (requires QEMU) (y/n) "
 if %run% EQU y qemu-system-i386 -kernel debug/mountainos.bin
 pause
