1f61dda67eb78415a4cacb105345154b65412faf
zHoop <gd.dashorrami@gmail.com>
2020-02-09 16:38:52 +0200

    Change update_cursor function

--- src/Drivers/VGA.c
+++ src/Drivers/VGA.c
@@ -18,27 +18,11 @@ uint16_t* terminal_buffer;
 
 void update_cursor(uint16_t x, uint16_t y)
 {
-	asm("mov bx, %0\n"
-		"mov ax, %1\n"
-		"mov dl, 80\n"
-		"mul dl\n"
-		"add bx, ax\n"
-		"mov dx, 0x03D4\n"
-		"mov al, 0x0F\n"
-		"out dx, al\n"
-		"inc dl\n"
-		"mov al, bl\n"
-		"out dx, al\n"
-		"dec dl\n"
-		"mov al, 0x0E\n"
-		"out dx, al\n"
-		"inc dl\n"
-		"mov al, bh\n"
-		"out dx, al\n"
-		"ret\n"
-		:
-		: "r" (x), "r" (y)
-		:);
+	uint16_t pos = y * VGA_WIDTH + x;
+	outportb(0x3D4, 0x0F);
+	outportb(0x3D5, (uint8_t) (pos & 0xFF));
+	outportb(0x3D4, 0x0E);
+	outportb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
 }
 
 void terminal_initialize(void)
